890f04c1f3127c94f307f26b8555eb4ed453e07c
Denis Almeida <denis.almeida@infobase.com.br>
2024-10-20 04:43:00 -0300

    :rocket: launch zion custom theme

---
+++ app/design/frontend/Zion/default/registration.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright © 2024 Denis Almeida. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Zion/default',
+    __DIR__
+);
